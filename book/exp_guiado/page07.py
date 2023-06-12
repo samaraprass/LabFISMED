2d57293db787b07f5dec53fb2a2eff12ac94ab46
Samara Prass dos Santos <92558250+samaraprass@users.noreply.github.com>
2023-06-12 17:45:19 -0300

    Update page07.py

--- book/exp_guiado/page07.py
+++ book/exp_guiado/page07.py
@@ -3,10 +3,10 @@ import streamlit as st
 def page7_func():
     st.subheader("Conclusão sobre o experimento de atenuação da radiação na matéria...")
 
-    st.image('book/Slide7.png', use_column_width=True)
-    st.image('book/Slide8.png', use_column_width=True)
-    st.image('book/Slide9.png', use_column_width=True)
-    st.image('book/Slide10.png', use_column_width=True)
-    st.image('book/Slide11.png', use_column_width=True)
+    st.image('book/Slide7.PNG', use_column_width=True)
+    st.image('book/Slide8.PNG', use_column_width=True)
+    st.image('book/Slide9.PNG', use_column_width=True)
+    st.image('book/Slide10.PNG', use_column_width=True)
+    st.image('book/Slide11.PNG', use_column_width=True)
 
     return
