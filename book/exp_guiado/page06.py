1611dac8b6a1a8628f5fa5e2988c3a434a55ff58
Samara Prass dos Santos <92558250+samaraprass@users.noreply.github.com>
2024-03-06 17:09:37 -0300

    Update page06.py - add 2nd experiment

--- book/exp_guiado/page06.py
+++ book/exp_guiado/page06.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import base64
+import numpy as np
+import core.texts_image as TXT_IMG
+import matplotlib.pyplot as plt
 
 def page6_func():
     st.subheader("Contraste da imagem...")
@@ -53,5 +56,41 @@ def page6_func():
     # with c1:
     st.image('book/Slide5.PNG', use_column_width=True)      
     st.image('book/Slide6.PNG', use_column_width=True)
+
+    st.markdown('')
+    st.markdown('''
+        <style>div.card_f2{background-color: rgba(166, 193, 237, 0.3);  text-align:justify; padding: 20px; border-radius: 3px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); transition: 0.3s;}</style>
+        <div class="card_f2">
+        <div class="container">
+        <h6> Agora vamos simular o comportamento dos raios X ao atravessarem diferentes materiais, considerando uma energia de 17 keV. Selecione um dos materiais abaixo para visualizar a intensidade de raios X que atravessam 20 cm de espessura do material escolhido. </h6>
+        </div></div>
+        ''', unsafe_allow_html=True)
+    st.write(' ')
+
+    material = st.selectbox("Defina o material que deseja utilizar na simulação: ",
+                                    ("Alumínio", 'Chumbo', 'Osso', 'Tecido Mole', 'Ar'))
+    
+    if material == "Alumínio":
+        coef = 6.21350365
+
+    if material == 'Chumbo':
+        st.info("Curiosidade: Você sabia que o chumbo é amplamente utilizado para blindagem de radiações ionizantes como "
+        "os raios X e gama? Ele é utilizado na proteção radiológica através de aventais, vidros e portas. Observe "
+        "que ele atenua praticamente todos os raios X com uma pequena espessura de material.")
+        coef = 136.490736
+
+    if material == "Osso":
+        coef = 6.88377316
+    
+    if material == "Tecido Mole":
+        coef = 1.30109489
+    
+    if material == "Ar":
+        coef = 0.001917519
+
+    porc = np.exp(-coef * 0.20)
+
+    TXT_IMG.title('\n RESULTADO - A porcentagem de fótons que irá sensibilizar o filme de raios X é: ' + f'{porc*100:.4}' +
+                " %", 17, 'black')
    
     return
